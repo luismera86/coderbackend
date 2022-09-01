ffbba37628bcd7b426b6be59617fe95474791928
Luis Mera <luismera86@gmail.com>
2022-09-01 15:30:16 -0300

    Desafío 13 incompleto falta la parte de nginx

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const router = Router()
-
-router.get('/datos', (req, res) => {
-  res.send('Datos')
-})
-
-export default router
--- src/server.js
+++ src/server.js
@@ -1,25 +1,32 @@
 import cluster from 'cluster'
 import express from 'express'
 import os from 'os'
-import router from './routes'
 
 const app = express()
 const cpus = os.cpus()
+const PORT = process.argv[2] || 8081
+const isCluster = process.argv[3] === 'cluster'
 
 app.get('/', (req, res) => {
   console.log(`LLevo request al worker ${process.pid}`)
   res.send('todo ok')
 })
 
-if (cluster.isPrimary) {
-  cpus.map(() => cluster.fork())
+if (cluster.isPrimary && isCluster) {
+  // Devuelve true si cluster es un proceso principal o false si es un worker
+  cpus.map(() => cluster.fork()) // Iteramos por todos los cpus y por cada cpu crea un subprocess
   cluster.on('exit', worker => {
-    console.log(`Worker ${worker.process.pid} died`)
-    cluster.fork()
+    console.log(`Worker ${worker.process.pid} died. Date: ${new Date().toLocaleDateString()}`)
+    cluster.fork() // Crea un nuevo proceso en caso de que se baje un worker
   })
 } else {
-  app.use('/', router)
-  app.listen(3000, () => {
-    console.log(`Escuchando el puerto 3000 - worker: ${process.pid}`)
+  app.listen(PORT, () => {
+    console.log(`Escuchando el puerto ${PORT} - Process ID: ${process.pid}. Date: ${new Date().toLocaleDateString()}`)
   })
 }
+
+// Muestra en el navegador el numero de procesos ejecutados
+
+app.get('/info', (req, res) => {
+  res.send(`Número de procesos ${cpus.length}`)
+})
