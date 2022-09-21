9249d3c3e582e7058519a54bf3f2ff1706878b1a
Luis Mera <luismera86@gmail.com>
2022-09-21 01:53:21 -0300

    Tercera entrega: products completado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,7 +4,7 @@ import logger from '../utils/logger.js'
 export const getProducts = async (req, res) => {
   try {
     const products = await Product.find()
-    res.status(200).json(products)
+    res.render('products', { products })
   } catch (error) {
     logger.info('error', error)
     res.status(404).json({ message: error.message })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import loginRouter from './loginRouter.js'
+import productsRouter from './productsRouter.js'
 import registerRouter from './registerRouter.js'
 import usersRouter from './usersRoutes.js'
 
@@ -7,5 +8,6 @@ const routes = Router()
 routes.use('/user', usersRouter)
 routes.use('/register', registerRouter)
 routes.use('/login', loginRouter)
+routes.use('/products', productsRouter)
 
 export default routes
